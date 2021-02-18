128d32e41f845bc706f089e97d90380afac644ce
cevherbakan <cevherbakan@gmail.com>
2021-02-18 23:36:36 +0300

    created example

---
+++ Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+
+        public int fiyat { get; set; }
+    }
+}
